caff961e0a8ec757bac673fa773d932052088c86
daisuke.nagamatsu <mugiblog5656@gmail.com>
2021-08-24 09:11:06 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
     get 'items/search'
     root to: 'items#search'
+    
+    get 'signup', to: 'users#new'
+    resources :users, only: [:index, :show, :create]
+
 end
